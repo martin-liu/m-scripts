c91f104bfaeec160f2faf893a69b0d47961809b9
Martin Liu <martinliu@lyft.com>
2022-12-24 21:32:09 -0800

    update

--- shell/install.sh
+++ shell/install.sh
@@ -2,17 +2,17 @@
 
 ## emacs
 brew tap d12frosted/emacs-plus && \
-    brew install emacs-plus@29 --with-native-comp --with-debug
+    brew install emacs-plus@29 --with-native-comp --with-poll --with-debug
 
 ## Alacritty terminal emulator
 brew install --cask alacritty
 
 ## tools
 brew install \
-     pyenv jenv \
+     pyenv jenv `# envs` \
      coreutils gnu-sed gnu-tar `# gnu utils` \
      starship ripgrep bat exa navi git-delta `# rust cli tools` \
-     fzf tmux reattach-to-user-namespace `# others`
+     fzf tmux reattach-to-user-namespace graphviz `# others`
 
 ## Fira Font
 brew tap homebrew/cask-fonts && brew install font-fira-code-nerd-font
