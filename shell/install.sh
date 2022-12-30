1d6b03d7ccb36fc16e2e36d2e503be590ff7cea7
Martin Liu <martinliu@lyft.com>
2022-12-29 21:16:19 -0800

    add rust tools

--- shell/install.sh
+++ shell/install.sh
@@ -11,7 +11,7 @@ brew install --cask alacritty
 brew install \
      pyenv jenv `# envs` \
      coreutils gnu-sed gnu-tar `# gnu utils` \
-     starship ripgrep bat exa navi git-delta zellij `# rust cli tools` \
+     starship ripgrep bat exa navi git-delta zellij tealdeer dust bottom fd `# rust cli tools` \
      fzf tmux reattach-to-user-namespace graphviz `# others`
 
 ## Fira Font
