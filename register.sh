4760ed123a905715ccce8d75a78fc72e098a059a
Martin Liu <hflhmartin@gmail.com>
2024-04-23 16:31:32 -0700

    add python env auto activate/deactivate

--- register.sh
+++ register.sh
@@ -5,10 +5,11 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 
 source $DIR/shell/util.sh
 source $DIR/shell/env.sh
-source $DIR/shell/zsh.sh
 source $DIR/shell/config.sh
 source $DIR/shell/alias.sh
 source $DIR/shell/tools.sh
+# zsh options at last, otherwise some hooks may cause incorrect state
+source $DIR/shell/zsh.sh
 
 # Don't end with errors.
 true
