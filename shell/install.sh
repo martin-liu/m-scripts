ce520f4b1a88c72b504b8afcb0b5e8da595213ac
Martin Liu <hflhmartin@gmail.com>
2025-04-23 10:16:25 -0700

    use basedpyright rather than pyright

--- shell/install.sh
+++ shell/install.sh
@@ -19,7 +19,7 @@ brew install \
      orbstack rye `# envs` \
      coreutils gnu-sed gnu-tar `# gnu utils` \
      rustup rust-analyzer starship zoxide ripgrep bat eza git-delta zellij tealdeer dust bottom fd `# rust cli tools` \
-     pyright pnpm cmake hr git fzf zsh-syntax-highlighting libvterm graphviz tree-sitter pandoc yq `# others`
+     basedpyright pnpm cmake hr git fzf zsh-syntax-highlighting libvterm graphviz tree-sitter pandoc yq `# others`
 
 ## Fira Font
 brew tap homebrew/cask-fonts && brew install font-fira-code
